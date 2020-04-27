962067900d6d224ff00a923aa35a5777ce20231f
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-28 05:39:44 +0700

    #3 - Update Pegawai Model

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pegawai extends Model
 {
-    //
+    protected $table = "pegawai";
+
+    protected $fillable = ['nama', 'email', 'alamat', 'telepon', 'pekerjaan'];
 }
