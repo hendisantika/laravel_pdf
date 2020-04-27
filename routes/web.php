3b3029341a78527c08033f36bc9540eafb376cbf
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-28 05:48:41 +0700

    #7 - Add pegawai & pdf route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pegawai', 'PegawaiController@index');
+Route::get('/pegawai/cetak_pdf', 'PegawaiController@cetak_pdf');
